01d596b78986853885dde0f4e2e2bde99a329a03
Mahmoud Abas <mmabas77@gmail.com>
2021-05-08 06:23:10 +0200

    Main World Try #1

--- PACKMAN/app/src/main/java/com/csheros/packman/view/MainFragment.java
+++ PACKMAN/app/src/main/java/com/csheros/packman/view/MainFragment.java
@@ -2,9 +2,13 @@ package com.csheros.packman.view;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,7 +16,12 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.csheros.packman.R;
+import com.csheros.packman.engine.Creature;
+import com.csheros.packman.engine.Node;
+import com.csheros.packman.engine.NodeMap;
 import com.csheros.packman.viewmodel.MainViewModel;
+
+import java.util.List;
 
 public class MainFragment extends Fragment {
 
@@ -40,15 +49,69 @@ public class MainFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
         mViewModel = new ViewModelProvider.
                 AndroidViewModelFactory(getActivity().getApplication())
                 .create(MainViewModel.class);
-//        mViewModel.getNodeMapLiveData().observe(getViewLifecycleOwner(),
-//                nodeMap -> {
-//
-//                });
-//
-//        mViewModel.createNodeMap(0);
+
+        mViewModel.getNodeMapLiveData().observe(getViewLifecycleOwner(),
+                this::nodeMapReceived
+        );
+
+        mViewModel.getCurrentLevelLiveData().observe(getViewLifecycleOwner(),
+                level -> {
+                    mViewModel.createNodeMap(level);
+                });
+
+    }
+
+    private void nodeMapReceived(NodeMap nodeMap) {
+        List<Node[]> rows = nodeMap.getNodesMap();
+        for (Node[] row : rows) {
+            // Linear for row
+            LinearLayout layoutRow = createRowLinear();
+            // add to row linear
+            for (Node node : row) {
+                // One square
+                LinearLayout nodeLayout = createNodeLayout(node);
+                layoutRow.addView(nodeLayout);
+            }
+        }
+    }
+
+    private LinearLayout createNodeLayout(Node node) {
+        // Create Node Layout
+        LinearLayout nodeView = createRowLinear();
+        nodeView.setGravity(Gravity.CENTER);
+
+        // Add Images
+        for (Creature creature : node.getCreatures()) {
+            ImageView img = new ImageView(getContext());
+            LayoutParams params = new LayoutParams(
+                    LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+            img.setLayoutParams(params);
+
+            switch (creature.getType()) {
+                default:
+                    img.setImageResource(R.drawable.ic_dot);
+            }
+
+            nodeView.addView(img);
+        }
+
+        return nodeView;
+    }
+
+    private LinearLayout createRowLinear() {
+        LinearLayout layoutRow = new LinearLayout(getContext());
+        layoutRow.setOrientation(LinearLayout.HORIZONTAL);
+        layoutRow.setLayoutParams(
+                new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.MATCH_PARENT
+                )
+        );
+        return layoutRow;
     }
 
 }
--- PACKMAN/app/src/main/java/com/csheros/packman/viewmodel/MainViewModel.java
+++ PACKMAN/app/src/main/java/com/csheros/packman/viewmodel/MainViewModel.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 
 import com.csheros.packman.engine.NodeMap;
 import com.csheros.packman.repo.MapRepo;
@@ -13,11 +14,13 @@ import org.jetbrains.annotations.NotNull;
 
 public class MainViewModel extends AndroidViewModel {
 
+    private final MutableLiveData<Integer> currentLevelLiveData;
     private final MapRepo mapRepo;
 
     public MainViewModel(@NonNull @NotNull Application application) {
         super(application);
         mapRepo = MapRepo.getInstance(application);
+        currentLevelLiveData = new MutableLiveData<>(1);
     }
 
     public void createNodeMap(int level) {
@@ -28,4 +31,7 @@ public class MainViewModel extends AndroidViewModel {
         return mapRepo.getNodeMapLiveData();
     }
 
+    public LiveData<Integer> getCurrentLevelLiveData() {
+        return currentLevelLiveData;
+    }
 }
