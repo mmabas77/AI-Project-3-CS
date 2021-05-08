6ef2a74374e370e0c0dbaf7d0f00ba25cbdf7d75
Mahmoud Abas <mmabas77@gmail.com>
2021-05-08 07:37:11 +0200

    Allow creature to choose asset

--- PACKMAN/app/src/main/java/com/csheros/packman/engine/Creature.java
+++ PACKMAN/app/src/main/java/com/csheros/packman/engine/Creature.java
@@ -1,5 +1,6 @@
 package com.csheros.packman.engine;
 
+import com.csheros.packman.R;
 import com.csheros.packman.utils.Direction;
 
 import lombok.Data;
@@ -45,6 +46,29 @@ public class Creature {
         }
     }
 
+    public int getImgAsset() {
+        switch (this.getType()) {
+            case PACK_MAN:
+                if (currentDirection == Direction.UP)
+                    return R.drawable.ic_packman_up;
+                else if (currentDirection == Direction.DOWN)
+                    return R.drawable.ic_packman_down;
+                else if (currentDirection == Direction.LEFT)
+                    return R.drawable.ic_packman_left;
+                else
+                    return R.drawable.ic_packman_right;
+            case EVIL_CREATURE:
+                return isReversed() ?
+                        R.drawable.ic_gohat_reversed :
+                        R.drawable.ic_gohat;
+            case MASTER_POINT:
+                return R.drawable.ic_dot_master;
+            case POINT:
+                return R.drawable.ic_dot;
+            default:
+                return R.drawable.square;
+        }
+    }
 
     public enum Type {
         PACK_MAN, EVIL_CREATURE, POINT, MASTER_POINT, BLOCK
