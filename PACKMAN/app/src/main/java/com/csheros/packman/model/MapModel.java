5f39497a7a333e7687703765182c00bb11744f32
Mahmoud Abas <mmabas77@gmail.com>
2021-05-08 06:29:13 +0200

    Fix Map live data is null

--- PACKMAN/app/src/main/java/com/csheros/packman/model/MapModel.java
+++ PACKMAN/app/src/main/java/com/csheros/packman/model/MapModel.java
@@ -33,6 +33,7 @@ public class MapModel {
 
     private MapModel(Application application) {
         this.application = application;
+        nodeMapLiveData = new MutableLiveData<>();
     }
 
     /**
