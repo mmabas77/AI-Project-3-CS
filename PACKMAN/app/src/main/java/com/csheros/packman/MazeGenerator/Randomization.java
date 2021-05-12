7659a74932ac30aa52aff3b43d4c2c52b55fcd3f
Abdelrahman <abdokh950@gmail.com>
2021-05-13 01:12:34 +0200

    Generator finshed

--- PACKMAN/app/src/main/java/com/csheros/packman/MazeGenerator/Randomization.java
+++ PACKMAN/app/src/main/java/com/csheros/packman/MazeGenerator/Randomization.java
@@ -16,11 +16,15 @@ import java.util.StringTokenizer;
 public class Randomization {
 
     private int n ;
-    public Randomization(int n ){
-        this.n =n ;
-    }
 
-    class Node {
+
+    private int xx[]={0 ,0 , -2 ,2 , 0 };
+    private int yy[]={2 ,-2 , 0 ,0 ,  0};
+    private int xx_2[]={0 ,0 , -1 ,1 , 0 };
+    private int yy_2[]={1 ,-1 , 0 ,0 ,  0};
+
+
+    private class Node {
         public int x , y ;
         public Node(int x ,int y ){
             this.x = x ;
@@ -29,9 +33,8 @@ public class Randomization {
         public boolean equals(Node other){
             return other.x ==x && other.y ==y ;
         }
-
     }
-    class Edge {
+    private class Edge {
         public int u , v ;
         public Edge(int u ,int v ){
             this.u = u ;
@@ -40,15 +43,17 @@ public class Randomization {
 
     }
 
+    public Randomization(int n ){
+        this.n =n ;
+    }
 
-    int xx[]={0 ,0 , -1 ,1 , 0 };
-    int yy[]={1 ,-1 , 0 ,0  ,0};
-    Node GetNodeFromID (int x ){
+    private Node GetNodeFromID (int x ){
         return new Node(x/n , x%n);
     }
-    int GetIDFromNode(Node s){
+    private  int GetIDFromNode(Node s){
         return n * s.x + s.y;
     }
+
     public char[][]GetMap(){
         char result[][] = new char[n][n];
         for (int i =0 ;i  <n ;i++)
@@ -64,25 +69,35 @@ public class Randomization {
             Edge currentEdge = edges.get(edges.size()-1);
             edges.remove(edges.size()-1);
             if (CheckNeighbors(dsu , currentEdge)){
-                result[GetNodeFromID(currentEdge.u).x][GetNodeFromID(currentEdge.u).y]='#';
-                result[GetNodeFromID(currentEdge.v).x][GetNodeFromID(currentEdge.v).y]='#';
+                result[GetNodeFromID(currentEdge.u).x][GetNodeFromID(currentEdge.u).y]='*';
+                result[GetNodeFromID(currentEdge.v).x][GetNodeFromID(currentEdge.v).y]='*';
                 dsu.merge(currentEdge.u , currentEdge.v);
+                for(int i =0 ;i  <n ;i+=1){
+                    for(int j =0 ;j<n ;j+=1){
+                        System.out.print(result[i][j]);
+                    }
+                    System.out.println();
+                }
+
             }
         }
 
         return result;
     }
+
+
     boolean check(Node node ){
         return(node.x >=0 && node.y>=0 && node.x < n && node.y < n  );
     }
+
     private boolean CheckNeighbors(DSU dsu, Edge currentEdge) {
         Node u = GetNodeFromID(currentEdge.u);
         Node v = GetNodeFromID(currentEdge.v);
         boolean res =true;
         for(int i =0 ;i  <5 ;i++){
             for(int j = 0 ; j <5 ;j++){
-                Node new_u = new Node(u.x+xx[i] , u.y+yy[i] );
-                Node new_v = new Node(v.x+xx[j] , v.y+yy[j] );
+                Node new_u = new Node(u.x+xx_2[i] , u.y+yy_2[i] );
+                Node new_v = new Node(v.x+xx_2[j] , v.y+yy_2[j] );
                 if(new_u.equals(new_v) || !check(new_u) || !check(new_v))
                     continue;
                 res = res && !dsu.isSame(GetIDFromNode(new_u) , GetIDFromNode(new_v));
@@ -90,8 +105,10 @@ public class Randomization {
             }
         }
         return res;
-
     }
+
+
+
 
     public ArrayList<Edge> GenerteEdges(int n)
     {
