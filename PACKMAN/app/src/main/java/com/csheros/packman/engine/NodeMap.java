22cf48a9452ebc2b59ce28cd2d23dfb041d1e76a
Mahmoud Abas <mmabas77@gmail.com>
2021-05-16 13:29:11 +0200

    Remove old way to create NodeMap

--- PACKMAN/app/src/main/java/com/csheros/packman/engine/NodeMap.java
+++ PACKMAN/app/src/main/java/com/csheros/packman/engine/NodeMap.java
@@ -25,26 +25,6 @@ public class NodeMap {
         this.packManDirection = Direction.STAND_STILL;
     }
 
-    public NodeMap(
-            MapSize mapSize,
-            NodePosition packManPosition,
-            NodePosition[] evilCreaturesPositions,
-            NodePosition[] blocksPositions,
-            NodePosition[] masterPointPositions
-    ) {
-        this.mapSize = mapSize;
-        this.nodesMap = new ArrayList<>();
-        this.packManDirection = Direction.STAND_STILL;
-        this.packManPosition = packManPosition;
-
-        constructMap(mapSize);
-        fillWithNodes(nodesMap);
-        setPackManPosition(packManPosition);
-        setEvilCreaturesPositions(evilCreaturesPositions);
-        setBlocksPositions(blocksPositions);
-        setMasterPointPositions(masterPointPositions);
-    }
-
 
     public List<Node> getAllNodes() {
         List<Node> nodes = new ArrayList<>();
@@ -73,50 +53,6 @@ public class NodeMap {
             this.packManPosition = nodePosition;
     }
 
-    private void constructMap(MapSize mapSize) {
-        for (int i = 0; i < mapSize.getHeight(); i++) {
-            nodesMap.add(new Node[mapSize.getWidth()]);
-        }
-    }
-
-    private void fillWithNodes(List<Node[]> nodesMap) {
-        for (int i = 0; i < nodesMap.size(); i++) {
-            Node[] row = nodesMap.get(i);
-            for (int j = 0; j < row.length; j++) {
-                row[j] = new Node(this, new NodePosition(i, j));
-                row[j].replaceCreatures(new Creature(Creature.Type.POINT));
-            }
-        }
-    }
-
-    private void setPackManPosition(NodePosition packManPosition) {
-        Node packManNode = getNodeByPosition(packManPosition);
-        packManNode.replaceCreatures(new Creature(Creature.Type.PACK_MAN));
-    }
-
-    private void setEvilCreaturesPositions(NodePosition[] evilCreaturesPositions) {
-        for (NodePosition evilCreaturePosition :
-                evilCreaturesPositions) {
-            Node evilCreatureNode = getNodeByPosition(evilCreaturePosition);
-            evilCreatureNode.replaceCreatures(new Creature(Creature.Type.EVIL_CREATURE));
-        }
-    }
-
-    private void setBlocksPositions(NodePosition[] blocksPositions) {
-        for (NodePosition blockPosition :
-                blocksPositions) {
-            Node blockNode = getNodeByPosition(blockPosition);
-            blockNode.replaceCreatures(new Creature(Creature.Type.BLOCK));
-        }
-    }
-
-    private void setMasterPointPositions(NodePosition[] masterPointPositions) {
-        for (NodePosition masterPointPosition :
-                masterPointPositions) {
-            Node masterPointNode = getNodeByPosition(masterPointPosition);
-            masterPointNode.replaceCreatures(new Creature(Creature.Type.MASTER_POINT));
-        }
-    }
 
     private Node getNodeByPosition(NodePosition packManPosition) {
         return this.nodesMap
--- PACKMAN/app/src/main/java/com/csheros/packman/utils/NodeMapGenerator.java
+++ PACKMAN/app/src/main/java/com/csheros/packman/utils/NodeMapGenerator.java
@@ -12,24 +12,6 @@ import java.util.Scanner;
 
 public interface NodeMapGenerator {
 
-    static NodeMap createStaticNodeMap() {
-        return new NodeMap(
-                new MapSize(7, 7),
-                new NodePosition(1, 4),
-                new NodePosition[]{
-                        new NodePosition(1, 1),
-                        new NodePosition(5, 1)
-                },
-                new NodePosition[]{
-                        new NodePosition(1, 2),
-                        new NodePosition(2, 2),
-                        new NodePosition(2, 3),
-
-                },
-                new NodePosition[]{new NodePosition(1, 3)}
-        );
-    }
-
     static NodeMap createDynamicNodeMap(int level) {
         // Todo : Implement this
         return createFromTxt(
